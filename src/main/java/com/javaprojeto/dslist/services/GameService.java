266dbdbf39e98a67fe5137974da1aeb0e118ca2d
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2025-08-23 13:55:02 -0300

    Get name by id

--- src/main/java/com/javaprojeto/dslist/services/GameService.java
+++ src/main/java/com/javaprojeto/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.javaprojeto.dslist.services;
 
+import com.javaprojeto.dslist.dto.GameDTO;
 import com.javaprojeto.dslist.dto.GameMinDTO;
 import com.javaprojeto.dslist.entities.Game;
 import com.javaprojeto.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,8 +16,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        return new GameDTO(gameRepository.findById(id).get());
     }
 }
